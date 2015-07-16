20f18c0b20d39b2b042fe0ff55f29db5ff4d1c68
alfin-akhret <alfin.akhret@gmail.com>
2015-07-16 13:03:50 +0000

    move REST request from services to models

--- app/js/models.js
+++ app/js/models.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('BzApp')
-    .service('m_tap', function(){
-        this.tapPos = [
-            [-6.2293465,106.829518],
-            [-6.2296700,106.829518],
-            [-6.2299900,106.829518]    
-        ]
-    })
+    .service('m_tap', function($http, $q){
+        return {
+            tapCoordinate : function(){
+                var d = $q.defer();
+				$http.get("app/dummies/taps.json")			// TODO: this should called backend scripts
+					.success(function(response){
+						d.resolve(response);
+				});
+				return d.promise;   
+            }
+        };
+    });
--- app/js/services.js
+++ app/js/services.js
@@ -13,61 +13,50 @@ angular.module('BzApp')
 	// and place TAP marker on map
 	.service('s_tap', function(m_tap){
 		
-		// get TAP's coordinate
-		this.getTapPos = function(){
-			return m_tap.tapPos;
-		}
-		
 		// place TAP's marker on map
 		this.placeTapMarker = function(map){
 			
 			var tapIcon = 'app/images/calendar-blue-circle.png';
 			
-			var tapPos = this.getTapPos();
-			
-			// place TAP on the map
-			for(var i = 0; i < tapPos.length; i++){
-				var marker = new google.maps.Marker({
-                   position: new google.maps.LatLng(tapPos[i][0], tapPos[i][1]),
-                   map: map,
-                   icon:tapIcon
-                });
-			}
-            
-		}
+			m_tap.tapCoordinate().then(function(r){
+				for(var i = 0; i < r.tapCoordinate.length; i++){
+						
+					var marker = new google.maps.Marker({
+		       			position: new google.maps.LatLng(r.tapCoordinate[i][0], r.tapCoordinate[i][1]),
+	                    map: map,
+ 	                    icon:tapIcon
+		            });
+				}
+			});
+		};
 		
 	})
 	
 	// s_cable service
 	// connect all TAP's with cable line
-	.service('s_cable', function(s_tap){
-		
-		// get cable coordinate based on TAP's position
-		this.getCablesPos = function(){
-			
-			var cables = [];
-			for(var i = 0; i < s_tap.getTapPos().length; i++){
-				cables.push(new google.maps.LatLng(s_tap.getTapPos()[i][0], s_tap.getTapPos()[i][1]));
-			}
-
-			return cables;
-		}
+	.service('s_cable', function(m_tap){
 		
 		// create cable route marker
 		// and place it on the map
 		this.placeCableRouteMarker = function(map){
 			
-			var cables = this.getCablesPos();
-			
-			var cablePath = new google.maps.Polyline({
-                path: cables,
-                geodesic: true,
-                strokeColor: 'blue',
-                strokeOpacity: 1.0,
-                strokeWeight: 1,
-                map:map
-            });
-		}
+			m_tap.tapCoordinate().then(function(r){
+				var cables = [];
+		
+				for(var i = 0; i < r.tapCoordinate.length; i++){
+					cables.push(new google.maps.LatLng(r.tapCoordinate[i][0], r.tapCoordinate[i][1]));
+				}
+				
+				var cablePath = new google.maps.Polyline({
+			        path: cables,
+		            geodesic: true,
+	                strokeColor: 'blue',
+	                strokeOpacity: 1.0,
+	                strokeWeight: 1,
+		            map:map
+		        });
+			});
+		};
 		
 	})
 	
@@ -84,7 +73,7 @@ angular.module('BzApp')
 		            var mapOptions = {
 		                center: latLng,
 		                zoom: 18
-		            }
+		            };
 		            
 		            // create map
 		            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -96,6 +85,6 @@ angular.module('BzApp')
 		            s_cable.placeCableRouteMarker(map);
 
 				}	
-			}
-		}
+			};
+		};
 	});
