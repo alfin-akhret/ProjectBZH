19091ad621c0ab7b7b09111482d0b2aeb3e4b31a
alfin-akhret <alfin.akhret@gmail.com>
2015-07-30 08:33:53 +0000

    delete all

--- app/js/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-(function(){
-	angular.module('BzApp', ['BzApp.config']);
-	
-	angular.module('BzApp.config',[])
-	.constant('REQHEADER', {'Content-type': 'application/x-www-form-urlencoded'});
-     
-})();
